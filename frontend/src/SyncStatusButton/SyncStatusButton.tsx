790256114e80e7657681f35c18dc45604762113d
Brigham Andersen <brighambandersen@gmail.com>
2024-07-16 23:41:31 -0600

    Update refresh button

--- frontend/src/SyncStatusButton/SyncStatusButton.tsx
+++ frontend/src/SyncStatusButton/SyncStatusButton.tsx
@@ -1,4 +1,5 @@
-import LoadingSpinner from './LoadingSpinner';
+import LoadingSpinner from '../LoadingSpinner';
+import styles from './SyncStatusButton.module.css';
 
 interface SyncStatusButtonProps {
   isSyncing: boolean;
@@ -10,33 +11,16 @@ function SyncStatusButton(props: SyncStatusButtonProps) {
 
   return (
     <button
-      aria-label='Refresh'
+      aria-label={isSyncing ? 'Syncing' : 'Refresh'}
       disabled={isSyncing}
       onClick={performSync}
-      style={{
-        border: 'none',
-        backgroundColor: 'transparent',
-        borderRadius: '50%',
-        cursor: 'pointer',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '48px',
-        width: '48px'
-      }}
-      title='Refresh'
+      className={styles.syncStatusButton}
+      title={isSyncing ? 'Syncing' : 'Refresh'}
     >
       {isSyncing ? (
         <LoadingSpinner />
       ) : (
-        <svg
-          viewBox='0 0 24 24'
-          style={{
-            width: '24px',
-            height: '24px',
-            fill: 'rgb(95, 99, 104)'
-          }}
-        >
+        <svg className={styles.refreshSvg} viewBox='0 0 24 24'>
           <path d='M13 9v2h7V4h-2v2.74C16.53 5.07 14.4 4 12 4c-2.21 0-4.21.9-5.66 2.34S4 9.79 4 12c0 4.42 3.58 8 8 8 2.21 0 4.21-.9 5.66-2.34l-1.42-1.42A5.98 5.98 0 0 1 12 18c-3.31 0-6-2.69-6-6 0-1.65.67-3.15 1.76-4.24A5.98 5.98 0 0 1 12 6a6.01 6.01 0 0 1 5.19 3H13z' />
         </svg>
       )}
