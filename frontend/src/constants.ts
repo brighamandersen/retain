31596bc81785140845e0dcca250bb39cde731630
Brigham Andersen <brighambandersen@gmail.com>
2024-10-02 21:40:48 -0600

    Clean up frontend

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,6 +1,7 @@
 import { UnsavedNote } from './types';
 
-export const API_BASE_URL = 'http://localhost:3001';
+export const API_BASE_URL = 'https://retain-api.brighamandersen.com';
+// export const API_BASE_URL = 'http://localhost:3001'; // Uncomment this line and comment out the line above to hit local backend endpoints
 
 export const BLANK_NOTE: UnsavedNote = {
   title: '',
