f903fd2334dd847300bae7d18ef913ee587295ac
Brigham Andersen <brighambandersen@gmail.com>
2024-10-02 21:17:37 -0600

    Adjust base url

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,6 +1,6 @@
 import { UnsavedNote } from './types';
 
-export const API_BASE_URL = 'http://localhost:3000';
+export const API_BASE_URL = 'http://localhost:3001';
 
 export const BLANK_NOTE: UnsavedNote = {
   title: '',
