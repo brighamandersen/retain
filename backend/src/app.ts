59505514ddbe462db4764ca55fa0e5fb78ddadea
Brigham Andersen <brighambandersen@gmail.com>
2024-10-02 21:55:07 -0600

    Remove duplicate line

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -28,7 +28,6 @@ const clientUrl = isProduction
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: clientUrl, credentials: true }));
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 app.use(
   session({
     cookie: {
