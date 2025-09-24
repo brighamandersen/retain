0729f7985618c405b83d9b4b85651f28869bca97
Brigham Andersen <brighambandersen@gmail.com>
2025-09-24 09:50:59 -0600

    Update retain script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,18 +3,22 @@ set -euo pipefail
 
 echo "Deploying retain"
 
-sudo ln -sf /home/brig/code/retain/deploy/nginx/retain.conf /etc/nginx/conf.d/retain.conf
+# nginx
 
 cd /home/brig/code/retain/frontend
 npm run build
+
+sudo ln -sf /home/brig/code/retain/deploy/nginx/retain.conf /etc/nginx/conf.d/retain.conf
+
+sudo nginx -t
+sudo systemctl reload nginx
+
+# systemd
 
 sudo ln -sf /home/brig/code/retain/deploy/systemd/retain.service /etc/systemd/system/retain.service
 
 sudo systemctl daemon-reload
 sudo systemctl enable retain.service
 sudo systemctl restart retain.service
-
-sudo nginx -t
-sudo systemctl reload nginx
 
 echo "Deployment complete for retain"
